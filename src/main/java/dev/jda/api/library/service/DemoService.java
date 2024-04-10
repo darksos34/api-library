b0d5d50608972333ef50c81a4de3dd7ff6a35250
Jordy Coder <j.hamwijk34@gmail.com>
2024-04-10 08:32:46 +0200

    Add Javadoc to the service

--- src/main/java/dev/jda/api/library/service/DemoService.java
+++ src/main/java/dev/jda/api/library/service/DemoService.java
@@ -17,15 +17,35 @@ public class DemoService {
     private static final String DEMO_NOTFOUND = "Kon de demo niet vinden op basis van UUID";
     private final DemoRepository demoRepository;
 
+    /**
+     * Get a demo by its code from the database
+     * @param code the code of the demo
+     *             to get from the database
+     * @return  the demo with the given code
+     */
     public Demo getDemoByCode(String code) {
         return demoRepository.findByCode(code)
                 .orElseThrow(() -> new EntityNotFoundException(String.format("Demo met code '%s' is niet gevonden", code)));
     }
 
+    /**
+     * Get all demos from the database
+     *
+     * @param pageable the pageable object
+     *                 containing the page number and size
+     * @return  a pageable list of demos
+     */
     public Page<Demo> getAllDemosPageable(Pageable pageable) {
         return demoRepository.findAll(pageable);
     }
 
+    /**
+     * Save a demo to the database
+     *
+     * @param demo Creata a demo to save to the database
+     * @return  the saved demo
+     * @throws  DemoCodeExistsException if the code already exists
+     */
     public Demo saveDemo(Demo demo) {
         if (demoRepository.existsByCode(demo.getCode())) {
             throw new DemoCodeExistsException(demo.getCode());
@@ -33,6 +53,14 @@ public class DemoService {
         return demoRepository.save(demo);
     }
 
+    /**
+     * Update a demo by its given uuid to the database
+     *
+     * @param uuid of the demo to update
+     * @param demo  the demo to update
+     * @return  the updated demo
+     * @throws EntityNotFoundException if the demo is not found
+     */
     public Demo saveDemoByUuid(String uuid, Demo demo) {
         Demo existingDemo = demoRepository.findByUuid(uuid).orElseThrow(() -> new EntityNotFoundException(String.format(uuid, DEMO_NOTFOUND)));
 
@@ -43,6 +71,12 @@ public class DemoService {
         return demoRepository.save(existingDemo);
     }
 
+    /**
+     * Delete a demo by its uuid
+     *
+     * @param uuid of the demo to delete
+     * @throws EntityNotFoundException if the demo is not found
+     */
     public void deleteDemoByUuid(String uuid) {
       Demo demo = demoRepository.findByUuid(uuid)
                 .orElseThrow(() -> new EntityNotFoundException(String.format(uuid, DEMO_NOTFOUND)));
