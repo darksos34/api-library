dd0b4a69f1a05863ba546d3665d11e5a1a05673d
Jordy Coder <59865566+darksos34@users.noreply.github.com>
2026-02-03 14:00:55 +0100

    add github workflow

--- src/main/java/dev/jda/api/library/common/api/ApiKeyResponse.java
+++ src/main/java/dev/jda/api/library/common/api/ApiKeyResponse.java
@@ -1,4 +1,17 @@
 package dev.jda.api.library.common.api;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ApiKeyResponse {
+    private final String apiKey;
+    private final int length;
+
+    public ApiKeyResponse(String apiKey, int length) {
+        this.apiKey = apiKey;
+        this.length = length;
+    }
+
 }
--- src/main/java/dev/jda/api/library/common/constant/RequestPath.java
+++ src/main/java/dev/jda/api/library/common/constant/RequestPath.java
@@ -2,6 +2,8 @@ package dev.jda.api.library.common.constant;
 
 public class RequestPath {
     public static final String V1 = "/v1";
+    public static final String API = "/api";
+    public static final String KEY = "/key";
     public static final String USER = "/user";
     public static final String PROFILE = "/profile";
 
