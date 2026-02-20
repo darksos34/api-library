e02b88fd30c87ca90c483edc3a5ee1c0bff247e0
Jordy Coder <59865566+darksos34@users.noreply.github.com>
2026-02-20 08:14:24 +0100

    Update parser builder to Jwts.parser(), Update jjwt versions dependencies and fix JWT parsing error + update gitignore

--- src/main/java/dev/jda/api/library/common/security/util/JwtUtil.java
+++ src/main/java/dev/jda/api/library/common/security/util/JwtUtil.java
@@ -23,7 +23,7 @@ public class JwtUtil {
     }
 
     public String validateToken(String token) {
-        return Jwts.parserBuilder()
+        return Jwts.parser()
                 .setSigningKey(key)
                 .build()
                 .parseClaimsJws(token)
